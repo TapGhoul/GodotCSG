d45a01d12e245467ff43eb6bf81c19bdd6120e08
Andrew Silver <andrew.silver0@gmail.com>
2017-06-04 15:16:07 +0200

    Made a tool dock

--- addons/csg_node/plugin.gd
+++ addons/csg_node/plugin.gd
@@ -1,9 +1,14 @@
 tool
 extends EditorPlugin
 
+var dock
+
 func _enter_tree():
+	dock = preload("Dock.tscn").instance()
 	add_custom_type("CSGNode", "MeshInstance", preload("csg_node.gd"), preload("csg_node.png"))
+	add_control_to_bottom_panel(dock, "CSG Editor")
 	pass
 
 func _exit_tree():
+	remove_control_from_bottom_panel(dock)
 	pass
